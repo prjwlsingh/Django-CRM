ae6f06fb62903176938316b5d66ce65646635de5
Prajwal <prjwlsingh@gmail.com>
2023-08-23 21:21:44 +0530

    registration

--- website/views.py
+++ website/views.py
@@ -40,6 +40,6 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
 
-
-    return render(request, 'register.html',{})
+    return render(request, 'register.html', {'form':form})
